484e0454116a5ae31e82b2a6b64f0bb6f306e9c9
Bryce Neal <brycedneal@gmail.com>
2026-01-30 11:54:48 -0500

    Fix autotidy URLs

--- Formula/autotidy.rb
+++ Formula/autotidy.rb
@@ -9,12 +9,12 @@ class Autotidy < Formula
 
   on_macos do
     on_intel do
-      url "file:///Users/bryce/projects/autotidy/autotidy_#{version}_darwin_amd64.tar.gz"
+      url "https://github.com/prettymuchbryce/autotidy/releases/download/v#{version}/autotidy_#{version}_darwin_amd64.tar.gz"
       sha256 "5880c8b551fb0f87d63fc40551a691ff6cedbfad1a12334922505425f7d82476"
     end
 
     on_arm do
-      url "file:///Users/bryce/projects/autotidy/autotidy_#{version}_darwin_arm64.tar.gz"
+      url "https://github.com/prettymuchbryce/autotidy/releases/download/v#{version}/autotidy_#{version}_darwin_arm64.tar.gz"
       sha256 "18c059ab57efba59bf91b45c2f9c9ee64ca4adafd9b57c4aac99cb8bf61da943"
     end
   end
